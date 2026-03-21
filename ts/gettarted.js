4e9201f4fa37992ee12e04260b861143f971a37e
jer <mjlerba@gmail.com>
2026-03-21 12:50:10 +0200

    adding ts

---
+++ ts/gettarted.js
@@ -0,0 +1,4 @@
+var ponyNumber = 0;
+var ponyName = 'Victorine';
+//const pony: Pony = new Pony();
+console.log(ponyName);
---
+++ ts/gettarted.ts
@@ -0,0 +1,5 @@
+const ponyNumber: number = 0;
+const ponyName : string = 'Victorine';
+
+//const pony: Pony = new Pony();
+console.log(ponyName);
