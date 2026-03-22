a30b8438d45d810698ee7a47a0cd6520a027caab
jer <mjlerba@gmail.com>
2026-03-22 13:25:51 +0200

    shadow component

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
 class Pony extends HTMLElement{
   constructor(){
 	super();
-	  console.log('Iam a component');
+	  const shadow = this.attachShadow({ mode: 'open' });
+	  const title = document.createElement('h1');
+	  title.textContent = 'General Solda, a great fighter';
+
+	  shadow.appendChild(title);
+	  console.log('this component work');
   }
-	connectedCallback(){
-		this.innerHTML=`<h1> General Solda a Warrior </h1> `;
-	}
+
 }
 customElements.define('ns-pony', Pony)
