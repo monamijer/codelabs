1745fcfd4c0f86029890958787cff85e40e9e2b2
jer <mjlerba@gmail.com>
2026-03-22 16:28:36 +0200

    input in component

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,21 @@
 class Pony extends HTMLElement{
-  constructor(){
-	super();
-	  const template = document.querySelector('#pony-template');
-	  const clone = document.importNode(template.content, true)
-	  
-	  const shadow = this.attachShadow({ mode: 'open' });
-	  shadow.appendChild(clone);
-	  console.log('this component work');
+  static get observedAttributes(){
+	return ['name'];
+  }
+  attributeChangedCallback(name, oldValue, newValue){
+	if(name === 'name'){
+	  this.render(newValue)
+	}
+  }
+  connectedCallback(){
+	const name = this.getAttribute('name');
+	this.render(name);
+  }
+  render(name){
+	if(!this.shadowRoot){
+	  this.attachShadow({ mode: 'open' });
+	}
+	this.shadowRoot.innerHTML = `<h1> ${name}</h1>`;
   }
 
 }
