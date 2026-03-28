6b07456dee3a85e366e5582d41581dea6fdd2095
jer <mjlerba@gmail.com>
2026-03-28 17:26:24 +0200

    adding lazy loading route for admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'admin', loadChildren: ()=> import('./admin/admin.routes').then(m=>m.adminRoutes) }
+];
