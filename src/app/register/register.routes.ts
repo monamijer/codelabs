d0a2cef1ea80458b5f44335c769e738e9a226a9a
jer <mjlerba@gmail.com>
2026-03-28 18:30:53 +0200

    register router

---
+++ src/app/register/register.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from "@angular/router";
+import { Register } from "./register";
+
+export const registerRoutes: Routes = [{ path: 'register', loadComponent: ()=> import('./register').then(m=>m.Register)}]
