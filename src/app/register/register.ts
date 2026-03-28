c0843644301826bcc63563cdea92cbc38794a57c
jer <mjlerba@gmail.com>
2026-03-28 19:43:34 +0200

    feat: add and updating form for validation and error reports

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserModel } from '../models/usermodel';
 
 @Component({
   selector: 'ns-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
-  protected register(user: { username: string; password: string }): void{
-    console.log(user);
+
+  private readonly fb = inject(FormBuilder);
+
+  protected readonly userNameCtrl = this.fb.control('', [Validators.required, Validators.minLength(3)]);
+  protected readonly passwordCtrl = this.fb.control('', [Validators.required]);
+
+  protected readonly userForm = this.fb.group({
+    username: this.userNameCtrl,
+    password: this.passwordCtrl
+  })
+  protected setAnotherNinja(): void{
+    this.userNameCtrl.setValue('JB')
+  }
+
+  protected register(): void{
+    console.log(this.userForm.value);
   }
 
 }
