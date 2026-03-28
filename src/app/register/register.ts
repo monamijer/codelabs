47fbb9018d8aa9e132ec740d9fd6147bbf9cb6ab
jer <mjlerba@gmail.com>
2026-03-28 17:43:20 +0200

    create register component

---
+++ src/app/register/register.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ns-register',
+  imports: [],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {}
