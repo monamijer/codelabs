ed59d65b9fea5d2b03897ca28bbb8b1637ac58b2
jer <mjlerba@gmail.com>
2026-03-28 18:29:42 +0200

    routes for register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+import { Admin } from './admin/admin';
+import { Register } from './register/register';
 
 export const routes: Routes = [
+  {path: 'admin', component: Admin},
+  {path: 'register', component: Register}
 ];
