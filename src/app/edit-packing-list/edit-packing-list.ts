84ecd8b4b8ba2be407f9084db0fdeeca19f1c392
jer <mjlerba@gmail.com>
2026-03-29 10:23:55 +0200

    packlist added

--- src/app/edit-packing-list/edit-packing-list.ts
+++ src/app/edit-packing-list/edit-packing-list.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { KeyValuePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { NonNullableFormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'ns-edit-packing-list',
-  imports: [],
+  imports: [KeyValuePipe],
   templateUrl: './edit-packing-list.html',
   styleUrl: './edit-packing-list.css',
 })
-export class EditPackingList {}
+export class EditPackingList {
+  private readonly fb = inject(NonNullableFormBuilder);
+  protected readonly equipmentRecord = this.fb.record({
+    toothbrush : true
+  });
+  protected readonly packingListForm = this.fb.group({
+    equipments: this.equipmentRecord
+  });
+  protected addEquipment(equipment: string){
+    this.equipmentRecord.addControl(equipment, this.fb.control(true));
+  }
+  protected removeEquipment(equipment: string){
+    this.equipmentRecord.removeControl(equipment);
+  }
+}
