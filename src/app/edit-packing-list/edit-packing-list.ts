749b998ed5224ef1221217a462f1318698c6899c
jer <mjlerba@gmail.com>
2026-03-29 10:06:25 +0200

    edit packing list will user formRecord

---
+++ src/app/edit-packing-list/edit-packing-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ns-edit-packing-list',
+  imports: [],
+  templateUrl: './edit-packing-list.html',
+  styleUrl: './edit-packing-list.css',
+})
+export class EditPackingList {}
