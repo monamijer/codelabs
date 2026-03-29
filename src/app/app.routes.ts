2fdabdffe425fd3228e4aaa56d9e8bb7f7be1c4f
jer <mjlerba@gmail.com>
2026-03-29 10:07:39 +0200

    route for packlist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Admin } from './admin/admin';
 import { Register } from './register/register';
 import { Editblog } from './component/editblog/editblog';
+import { EditPackingList } from './edit-packing-list/edit-packing-list';
 
 export const routes: Routes = [
   {path: 'admin', component: Admin},
   {path: 'register', component: Register},
   {path: 'blog', component: Editblog}
+  {path: 'packlist', component: EditPackingList}
 ];
