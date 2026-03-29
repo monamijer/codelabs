c5702dc688b0d2ab8d09c7b69aba17377ecf0c03
jer <mjlerba@gmail.com>
2026-03-29 09:18:58 +0200

    feat: password confirmation matching add

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -13,13 +13,18 @@ export class Register {
   private readonly fb = inject(FormBuilder);
 
   protected readonly userNameCtrl = this.fb.control('', [Validators.required, Validators.minLength(3)]);
-  protected readonly passwordCtrl = this.fb.control('', [Validators.required]);
+  protected readonly passwordCtrl = this.fb.control('', Validators.required);
+  protected readonly confirmCtrl  = this.fb.control('', Validators.required);
   protected readonly birthdateCtrl= this.fb.control('', [Validators.required, Register.isOldEnough]);
 
+  protected readonly passwordGroup = this.fb.group(
+    { password: this.passwordCtrl, confirm: this.confirmCtrl},
+    { validators: Register.passwordMatch}
+  )
 
   protected readonly userForm = this.fb.group({
     username: this.userNameCtrl,
-    password: this.passwordCtrl,
+    passwordForm: this.passwordGroup,
     birthdate:this.birthdateCtrl
   })
 
@@ -27,6 +32,12 @@ export class Register {
     const birthDatePlus18 = new Date(control.value);
     birthDatePlus18.setFullYear(birthDatePlus18.getFullYear() + 18);
     return birthDatePlus18 < new Date() ? null : { tooYoung: true};
+  }
+
+  private static passwordMatch(group: AbstractControl<{password: string, confirm: string}>): ValidationErrors | null {
+    const password = group.value.password;
+    const confirm  = group.value.confirm;
+    return password === confirm ? null : { matchingError: true}
   }
 
   protected setAnotherNinja(): void{
