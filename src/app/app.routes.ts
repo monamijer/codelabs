7f96e1b71df8d74c682b327ed7060f3eb123e66d
jer <mjlerba@gmail.com>
2026-03-29 10:05:34 +0200

    blog for add or remove items in the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Admin } from './admin/admin';
 import { Register } from './register/register';
+import { Editblog } from './component/editblog/editblog';
 
 export const routes: Routes = [
   {path: 'admin', component: Admin},
-  {path: 'register', component: Register}
+  {path: 'register', component: Register},
+  {path: 'blog', component: Editblog}
 ];
