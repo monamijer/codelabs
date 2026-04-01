8b6a2ddc84ca7a475b24ed5dc4982c623c347a33
jer <mjlerba@gmail.com>
2026-04-01 07:23:12 +0200

    allow to run ng-profiler.timeChangeDetection

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, enableDebugTools } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig).catch(err => console.error(err));
+bootstrapApplication(App, appConfig)
+  .then(applicationRef=>{
+    const componentRef= applicationRef.components[0];
+    // allows to run `ng-profiler.timeChangeDetection();`
+    enableDebugTools(componentRef)
+  })
+	.catch(err => console.error(err));
